9d1293b15382bdc1988bfe9a232ca9fdbca64240
Surya Pratap <surya@skillenza.com>
2019-07-20 22:29:03 +0530

    backend whatsapp

--- backend/whatsapp.js
+++ backend/whatsapp.js
@@ -6,15 +6,15 @@ const accountSid = process.env.whatsappAccessId;
 const authToken = process.env.whatsappToken;
 const client = require('twilio')(accountSid, authToken);
 
-router.get('/', (req, res, next)=>{
-  client.messages
-      .create({
-         from: 'whatsapp:+14155238886',
-         body: 'Hello there!',
-         to: 'whatsapp:+919945551067'
-       })
-      .then(message => console.log(message.sid));
-  res.send("whatsapp working");
-})
+router
+  .get('/', (req, res, next)=>{
+    res.send("whatsapp working");
+  })
+
+router
+  .post('/', (req, res, next)=>{
+    res.send(req)
+  })
+
 
 module.exports = router;
