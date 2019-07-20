26e87a4fbb5a6174d518fbaf0b63ef5cfc130fae
Surya Pratap <surya@skillenza.com>
2019-07-21 02:37:36 +0530

    sql query

--- data_source/sql/create_tables.sql
+++ data_source/sql/create_tables.sql
@@ -1,3 +1,6 @@
+DROP DATABASE IF EXISTS everest_db;
+create database everest_db;
+USE everest_db;
 DROP TABLE IF EXISTS medical_facility;
 DROP TABLE IF EXISTS hospital;
 DROP TABLE IF EXISTS citizen_table;
@@ -16,9 +19,8 @@ CREATE TABLE IF NOT EXISTS medical_facility (user_id int NOT NULL AUTO_INCREMENT
 	 end_date VARCHAR(225),
 	 situation_affected VARCHAR(225),
 	 provider_name VARCHAR(225),
-	 PRIMARY KEY (user_id)
-​
-	  );
+	 PRIMARY KEY (user_id));
+
 CREATE TABLE IF NOT EXISTS hospital (hospital_id int NOT NULL AUTO_INCREMENT,
 	 name VARCHAR (225),
 	 code VARCHAR (225),
@@ -26,9 +28,7 @@ CREATE TABLE IF NOT EXISTS hospital (hospital_id int NOT NULL AUTO_INCREMENT,
 	 latitude float,
 	 longitude float,
 	 inventory_stock VARCHAR (225),
-	 PRIMARY KEY (hospital_id)
-​
-	  );
+	 PRIMARY KEY (hospital_id));
 CREATE TABLE IF NOT EXISTS citizen_table (citizen_id int NOT NULL AUTO_INCREMENT,
 	 name VARCHAR (225),
 	 age VARCHAR (225),
@@ -38,27 +38,25 @@ CREATE TABLE IF NOT EXISTS citizen_table (citizen_id int NOT NULL AUTO_INCREMENT
 	 blood_group VARCHAR (225),
 	 medical_condition VARCHAR (225),
 	 email VARCHAR (225),
-	 PRIMARY KEY (citizen_id)
-​
-	  );
+	 PRIMARY KEY (citizen_id));
+
 CREATE TABLE IF NOT EXISTS inventory_hospital (inventory_hospital_id int NOT NULL AUTO_INCREMENT,
 	 name VARCHAR (225),
 	 code VARCHAR (225),
 	 available_address VARCHAR (225),
-	 PRIMARY KEY (inventory_hospital_id)
-	  );
+	 PRIMARY KEY (inventory_hospital_id));
 ​
 CREATE TABLE IF NOT EXISTS policy (policy_id int NOT NULL AUTO_INCREMENT,
-	 overall_budget float,
-	 hr_salary float,
-	 operation_amount float,
-	 procurment_charge float,
-	 services_charge float,
-	 analysis_charge float,
-	 start_date VARCHAR (225),
-	 end_date  VARCHAR (225),
-	 develpoment_charge float,
-	 PRIMARY KEY (policy_id)
-​
-​
-	  );
+     overall_budget float,
+     hr_salary float,
+     operation_amount float,
+     procurment_charge float,
+     services_charge float,
+     analysis_charge float,
+     start_date VARCHAR (225),
+     end_date  VARCHAR (225),
+     develpoment_charge float,
+     PRIMARY KEY (policy_id)
+
+
+      );
