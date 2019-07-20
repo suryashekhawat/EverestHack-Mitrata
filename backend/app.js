6f451f3f4589a86286bb04c1fca01b71e6af50da
Surya Pratap <surya@skillenza.com>
2019-07-20 21:10:43 +0530

    backend

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,17 @@
 var express = require('express');
 
 var app = express();
+
 const port = 3000;
 
+var whatsappRoutes = require('./whatsapp.js');
+
+app.use('/whatsapp', whatsappRoutes);
 app.get('/', (req, res, next)=>{
     res.send("app running")
 });
+
+
 
 app.listen(port, () => {
   console.log(`app running on port ${port}`);
