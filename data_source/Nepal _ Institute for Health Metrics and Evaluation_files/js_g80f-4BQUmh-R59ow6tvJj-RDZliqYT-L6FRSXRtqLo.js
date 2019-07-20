63bd6900074183820d8693d544d8196aaa3a045b
Surya Pratap <surya@skillenza.com>
2019-07-20 13:43:51 +0530

    data init

---
+++ data_source/Nepal _ Institute for Health Metrics and Evaluation_files/js_g80f-4BQUmh-R59ow6tvJj-RDZliqYT-L6FRSXRtqLo.js
@@ -0,0 +1,12 @@
+(function($) {
+  $(document).ready(function(){
+		$('#pull-wrapper #menu-pull').click(function(e) {
+			e.preventDefault();
+			$(this).parent().next('.region-header').find('ul.menu').first().slideToggle('fast');
+		});
+		$('#pull-wrapper-section #menu-pull-section').click(function(e) {
+			e.preventDefault();
+			$('#secondary-menu-mobile').slideToggle('fast');
+		});
+	});
+})(jQuery);;
