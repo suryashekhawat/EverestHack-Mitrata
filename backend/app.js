8608bfec861da6d1a5e6ba0a0125dbd65533520d
Surya Pratap <surya@skillenza.com>
2019-07-20 23:49:26 +0530

    whatsapp messgae

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,11 @@
 require('dotenv').config()
 var express = require('express');
-
+var bodyParser = require('body-parser')
 var app = express();
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 
 const port = 3000;
 
--- backend/whatsapp.js
+++ backend/whatsapp.js
@@ -5,24 +5,66 @@ var router = express.Router();
 const accountSid = process.env.whatsappAccessId;
 const authToken = process.env.whatsappToken;
 const client = require('twilio')(accountSid, authToken);
+const whatsapp_number = 'whatsapp:+14155238886';
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
+function twimlMessage(message){
+  const twiml = new MessagingResponse();
+  return twiml.message(message);
+}
+
+
+function sendMessage(whatsapp_mobile, whatsapp_message){
+    client.messages
+    .create({
+       from: whatsapp_number,
+       body: `${whatsapp_message}`,
+       to: `whatsapp:+${whatsapp_mobile}`
+     })
+    .then(message => {
+      console.log(message.sid)
+      return message;
+    })
+    .catch(error => {
+      console.log(error);
+      return message;
+    });
+}
 router
   .get('/', (req, res, next)=>{
-    client.messages
-      .create({
-         from: 'whatsapp:+14155238886',
-         body: 'Hello there!',
-         to: 'whatsapp:+919945551067'
-       })
-      .then(message => console.log(message.sid));
-
-    res.send("whatsapp working");
+    var whatsapp_mobile = req.query.mobile;
+    var whatsapp_message = req.query.message;
+    sendMessage(whatsapp_mobile, whatsapp_message);
   })
 
 router
-  .post('/', (req, res, next)=>{
-    console.log(req);
-    res.send("new message")
+  .post('/', (req, res, next) => {
+    console.log(req.body);
+    var twiml;
+
+    var received_message = req.body.Body;
+    if (received_message == "hi" || received_message == "Hi"){
+      twiml = twimlMessage("hello");
+    }else{
+      switch(received_message){
+        case "my details":
+          twiml = twimlMessage("Please send your details in below format\nmy details:\nname: joy\nage: 12\ngendar: Male");
+        break;
+        case "my details:\nname: {{1}}\nage: {{2}}\ngendar: {{3}}":
+          twiml = twimlMessage("Thanks for sharing.");
+        break;
+
+        default:
+          twiml = twimlMessage("Could you say it again");
+        break;
+      }
+    }
+
+
+
+    res.writeHead(200, {'Content-Type': 'text/xml'});
+    res.end(twiml.toString());
+    // res.send("new message");
   })
 
 
