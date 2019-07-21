1240ae9c87ba7cd0b2c90c08ecdfa579e71f6d0f
Surya Pratap <surya@skillenza.com>
2019-07-21 09:09:28 +0530

    api

--- backend/apis.js
+++ backend/apis.js
@@ -66,15 +66,17 @@ router
     connection.query(`select * from policy where services_provided = ${req.query.services_provided}`, function(error, results, fields){
       var new_budget = req.query.new_budget
       console.log(req.query);
-      var raise_percentage = ((results.overall_budget[0] - results.overall_budget[1] )/results.overall_budget[0]) * 100
-      var increment = new_budget*raise_percentage
-      new_budget = new_budget + increment
-      var result = {overall_budget:new_budget, hr_salary:results.hr_salary[0]+increment, operation_amount:results.operation_amount[0]+increment, procurment_charge:results.procurment_charge+increment,services_charge:results.services_charge[0]+increment,analysis_charge:results.analysis_charge[0]+increment,develpoment_charge:results.develpoment_charge[0]+increment
-      }
-      res.send(
-        {
-          data: result
-        })
+      // var raise_percentage = ((results.overall_budget[0] - results.overall_budget[1] )/results.overall_budget[0]) * 100
+      // var increment = new_budget*raise_percentage
+      // new_budget = new_budget + increment
+      // var result = {
+      //   overall_budget:new_budget, hr_salary:results.hr_salary[0]+increment, operation_amount:results.operation_amount[0]+increment, procurment_charge:results.procurment_charge+increment,services_charge:results.services_charge[0]+increment,analysis_charge:results.analysis_charge[0]+increment,develpoment_charge:results.develpoment_charge[0]+increment
+      // };
+      //
+      // res.send({
+      //     data: result
+      //   })
+
     });
   });
 
