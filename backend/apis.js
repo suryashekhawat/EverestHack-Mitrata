2f1a029a144c483af359ad8dcfd87668833ad1ca
Sourabh <sourabh.pandey@hotelsoft.com>
2019-07-21 08:25:53 +0530

    testing backend

--- backend/apis.js
+++ backend/apis.js
@@ -65,14 +65,14 @@ router
     connection.query(`select * from policy where services_provided = ${req.param.services_provided}`, function(error, results, fields){
       console.log(error, results);
       var new_budget = req.param.new_budget
-      var raise_percentage = ((results.overall_budget[0] - results.overall_budget[1] )/results.overall_budget[0]) * 100
-      var increment = new_budget*raise_percentage
-      new_budget = new_budget + increment
-      var new_result = {overall_budget:new_budget, hr_salary:results.hr_salary[0]+increment, operation_amount:results.operation_amount[0]+increment, procurment_charge:results.procurment_charge+increment,services_charge:results.services_charge[0]+increment,analysis_charge:results.analysis_charge[0]+increment,develpoment_charge:results.develpoment_charge[0]+increment
+      // var raise_percentage = ((results.overall_budget[0] - results.overall_budget[1] )/results.overall_budget[0]) * 100
+      // var increment = new_budget*raise_percentage
+      // new_budget = new_budget + increment
+      // var new_result = {overall_budget:new_budget, hr_salary:results.hr_salary[0]+increment, operation_amount:results.operation_amount[0]+increment, procurment_charge:results.procurment_charge+increment,services_charge:results.services_charge[0]+increment,analysis_charge:results.analysis_charge[0]+increment,develpoment_charge:results.develpoment_charge[0]+increment
       }
       res.send(
         {
-          data: new_result
+          data: result
         })
     });
   });
